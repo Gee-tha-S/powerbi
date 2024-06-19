200ce281e6904c16adc0aceb3b8ee279f5d22081
Gee-tha-S <166114149+Gee-tha-S@users.noreply.github.com>
2024-06-20 00:56:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ from pandas import json_normalize
 import json
  
 # Load the JSON file
-json_file_path = 'code-scanning.json'
+json_file_path = 'fedex.json'
 with open(json_file_path, 'r') as f:
     data = json.load(f)
  
 # Normalize the nested JSON
 df = json_normalize(data)
- 
+
+# Define new data for additional columns
+repodf = pd.DataFrame(data, columns = ['repository.name'])
+column_data=[]
+eaiNumber=[]
+column_data=df['repository.name'].tolist()
+for reponame in column_data: 
+    eaiData=reponame.split("-")
+    eaiNumber.append(eaiData[1])
+    #print(eaiNumber)
+
+
+# Add multiple columns using dictionary assignment
+new_data = {'EAI Number': eaiNumber}
+df = df.assign(**new_data)
+
+#print(df)
 # Convert the DataFrame to a CSV file
-csv_file_path = 'code-scanning.csv'
+csv_file_path = 'code-scanning-results.csv'
 df.to_csv(csv_file_path, index=False)
  
 print(f"Nested JSON file has been converted to CSV file at: {csv_file_path}")
